15c80d994d1beea396136cf65984e1263e962846
Harsh <harshagrwl09@gmail.com>
2026-03-03 18:50:15 +0530

    added datatypes and variable naming rules

---
+++ 08. Programming Fundamentals in Js, Loops, Conditionals/datatypesdemo.js
@@ -0,0 +1,7 @@
+let productPrice = 152000;
+
+let productDiscount = 19;
+
+let productName = "IPhone 16 Pro Max(512GB, Orange)";
+
+let isAvailable = true;
---
+++ 08. Programming Fundamentals in Js, Loops, Conditionals/variablesintroduction.js
@@ -0,0 +1,9 @@
+let h = 9;
+
+let s = 14;
+
+let ss = 24;
+
+console.log(h)
+console.log(s)
+console.log(ss)
