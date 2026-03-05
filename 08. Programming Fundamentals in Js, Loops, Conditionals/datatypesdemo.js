689cce9f97477d543b829d46a9f5dc950e890381
Harsh <harshagrwl09@gmail.com>
2026-03-05 11:01:58 +0530

    added objects

--- 08. Programming Fundamentals in Js, Loops, Conditionals/datatypesdemo.js
+++ 08. Programming Fundamentals in Js, Loops, Conditionals/datatypesdemo.js
@@ -5,3 +5,19 @@ let productDiscount = 19;
 let productName = "IPhone 16 Pro Max(512GB, Orange)";
 
 let isAvailable = true;
+
+let minusZero = -0;
+
+console.log(minusZero);
+
+let x;
+
+let y = null;
+
+console.log(x);
+
+console.log(y);
+
+let veryBigNumber = 1234567890123456789012345678901234567890n;
+
+console.log(veryBigNumber);
---
+++ 08. Programming Fundamentals in Js, Loops, Conditionals/objectdemo.js
@@ -0,0 +1,8 @@
+let product = {
+    name: "IPhone 16 Pro Max(512GB, Orange)",
+    price: 152000,
+    discount: 19,
+    isAvailable: true
+}
+
+console.log(product)
