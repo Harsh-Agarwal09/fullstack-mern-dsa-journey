563c569d2374ebabfe197ccc019e65ccbecdbfeb
Harsh <harshagrwl09@gmail.com>
2026-03-09 23:11:47 +0530

    added more problems on condition

---
+++ 10. Problem on conditionals/validtriangle.js
@@ -0,0 +1,9 @@
+let a = 7;
+let b = 10;
+let c = 4;
+
+if (a + b > c && b + c > a && c + a > b) {
+    console.log("It can form a Triangle")
+} else {
+    console.log("It cannot form a Triangle")
+}
