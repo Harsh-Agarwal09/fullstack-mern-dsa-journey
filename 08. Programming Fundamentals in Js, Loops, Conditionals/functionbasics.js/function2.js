df4eb8e7448340729686c7d7e34eb6e08512e9cb
Harsh <harshagrwl09@gmail.com>
2026-03-22 11:13:43 +0530

    introducing functions basics

---
+++ 08. Programming Fundamentals in Js, Loops, Conditionals/functionbasics.js/function2.js
@@ -0,0 +1,11 @@
+function isEvenOrOdd(x) {
+    // x -> parameter -> placeholder -> expected input
+    if (x % 2 == 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+isEvenOrOdd(7);
+// 7 -> argument -> argument -> actual input
---
+++ 08. Programming Fundamentals in Js, Loops, Conditionals/functionbasics.js/function3.js
@@ -0,0 +1,7 @@
+function multiple(a, b) {
+    // a and b are expected inputs
+    console.log(a * b);
+}
+
+multiple(5, 10); // 5 & 10 both are actual values for a and b
+multiple(3, 7)  ; // 3 & 7 both are actual values for a and b
