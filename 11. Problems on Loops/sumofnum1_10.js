9f4a3236ba3ab87460926ec044346697b603c160
Harsh <harshagrwl09@gmail.com>
2026-04-03 22:36:23 +0530

    added Problems on Loops

---
+++ 11. Problems on Loops/sumofnum1_10.js
@@ -0,0 +1,16 @@
+// Write a program to calculate sum of all the no. from 1 - 10.
+
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+    sum += i;
+}
+console.log('Sum of all no. from 1 - 10 is: ' + sum);
+
+// Using while loop
+let sum1 = 0;
+let j = 1;
+while (j <= 10) {
+    sum1 += j;
+    j++;
+}
+console.log('Sum of all no. from 1 - 10 is: ' + sum1);
